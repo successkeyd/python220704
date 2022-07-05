ea7566dac10ff659055b013f86d23585ee52be7a
successkeyd <successkey@daeduck.com>
2022-07-05 16:12:06 +0900

    상속

--- 상속01.py
+++ 상속01.py
@@ -1,12 +1,16 @@
+#부모 클래스 정의
 class Person:
+    #초기화 매서드
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
+    #인스턴스 메서드
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
+#자식 클래스 정의(대학생)
 class Student(Person):
+    #상속받은 메서드를 재정의(덮어쓰기, override)
     def __init__(self, name, phoneNumber, subject, studentID):
         self.name = name
         self.phoneNumber = phoneNumber
@@ -15,7 +19,8 @@ class Student(Person):
 
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
+s = Student("이순신", "010-111-1234", "빅데이터", "191122")
+#내부값을 딕셔너리 형태
 print(p.__dict__)
 print(s.__dict__)
 
