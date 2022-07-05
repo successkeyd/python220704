7893cb5844cb3a3264e598f66f416304b2f4714b
successkeyd <successkey@daeduck.com>
2022-07-05 10:12:02 +0900

    함수 연습4

--- function3.py
+++ function3.py
@@ -12,3 +12,45 @@ def intersect(prelist, postlist):
 
 #호출
 print(intersect("HAM","SPAM"))
+
+#지역변수와 전역변수
+x = 1
+def func1(a):
+    return a+x
+
+#호출
+print(func1(1))
+
+def func2(a):
+    x = 2
+    return a+x
+
+#호출
+print(func2(1))
+
+print("---불변형식---")
+a = 1.2
+print(id(a))
+a = 2.3
+print(id(a))
+
+print("---가변형식---")
+lst = [1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+#객체는 참조를 통해 복사(입력+출력)
+wordlist = ["J","A","M"]
+
+#함수 정의
+def change(x):
+    #내부에서 객체의 복사본 생성(Deep Copy)
+    x1 = x[:]
+    x1[0] = "H"
+    print("내부:",x1)
+
+#호출
+change(wordlist)
+print("함수 호출후:", wordlist)
+
