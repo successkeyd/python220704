d271ebe37d2cf11b8a32b86fcddb10d7228cf74b
successkeyd <successkey@daeduck.com>
2022-07-06 11:10:39 +0900

    예외 처리 연습

--- 상속01.py
+++ 상속01.py
@@ -16,12 +16,18 @@ class Student(Person):
         self.phoneNumber = phoneNumber
         self.subject = subject
         self.studentID = studentID
+    #상속 받은 메서드를 재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1}".format(self.name, self.phoneNumber))
+        print("Info(")
 
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "빅데이터", "191122")
 #내부값을 딕셔너리 형태
-print(p.__dict__)
-print(s.__dict__)
+# print(p.__dict__)
+# print(s.__dict__)
+p.printInfo()
+s.printInfo()
 
 
